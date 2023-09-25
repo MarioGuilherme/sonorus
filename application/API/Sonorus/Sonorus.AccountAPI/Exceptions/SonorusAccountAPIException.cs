892343bd94cc2f855682f1698904f5c09e5f04c1
MárioGuilherme <marioguilhermedev@gmail.com>
2023-09-24 21:00:51 -0300

    fix: correção de referencias de classe de exceção

--- application/API/Sonorus/Sonorus.AccountAPI/Exceptions/SonorusAccountAPIException.cs
+++ application/API/Sonorus/Sonorus.AccountAPI/Exceptions/SonorusAccountAPIException.cs
@@ -2,11 +2,11 @@
 
 namespace Sonorus.AccountAPI.Exceptions;
 
-public class SonorusSonorusAccountAPIException : Exception {
+public class SonorusAccountAPIException : Exception {
     public int StatusCode { get; }
     public List<FieldError>? Errors { get; }
 
-    public SonorusSonorusAccountAPIException(string message, int statusCode, List<FieldError>? errors = null) : base(message) {
+    public SonorusAccountAPIException(string message, int statusCode, List<FieldError>? errors = null) : base(message) {
         this.StatusCode = statusCode;
         this.Errors = errors;
     }
