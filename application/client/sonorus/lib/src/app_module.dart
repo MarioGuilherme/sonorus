49f670813247bf5e729ee800d94f6004f1440bff
MárioGuilherme <marioguilhermedev@gmail.com>
2023-11-19 10:39:54 -0300

    refacotr: módulo de teste removido

--- application/client/sonorus/lib/src/app_module.dart
+++ application/client/sonorus/lib/src/app_module.dart
@@ -8,7 +8,6 @@ import "package:sonorus/src/modules/base/business/business_module.dart";
 import "package:sonorus/src/modules/base/marketplace/marketplace_module.dart";
 import "package:sonorus/src/modules/chat_realtime/chat_realtime_module.dart";
 import "package:sonorus/src/modules/core/core_module.dart";
-import "package:sonorus/src/modules/teste/teste_module.dart";
 
 class AppModule extends Module {
   @override
@@ -21,7 +20,6 @@ class AppModule extends Module {
     r.module("/", module: BaseModule());
     r.module("/marketplace/", module: MarketplaceModule());
     r.module("/business/", module: BusinessModule());
-    r.module("/teste/", module: TesteModule());
     r.module("/user/", module: UserModule());
     r.module("/chat/", module: ChatRealtimeModule());
     r.module("/login/", module: LoginModule());
